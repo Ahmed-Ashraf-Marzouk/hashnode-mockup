e630c3bdc211e4eef4246493de3173399c91cf0b
Kareem Amr Younes <official.kyounes@gmail.com>
2022-07-18 00:33:30 +0200

    Add boilerplate files

---
+++ boilerplate/js/import.js
@@ -0,0 +1,19 @@
+const imports_tag = document.querySelector("#imports")
+
+imports_tag.insertAdjacentHTML("afterend",`
+<!-- CSS -->
+<link rel="stylesheet" href="css/root.css">
+
+<!-- FONTS  -->
+    <!-- Manrope Font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Inter Font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+`)
+
+imports_tag.remove()
